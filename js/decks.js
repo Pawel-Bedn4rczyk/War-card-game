88546d72451b4b69e8a46b12fb5e3d33406ca9c6
Pawel-Bedn4rczyk <Pawel-Bednarczyk-lca@protonmail.com>
2018-10-30 14:20:48 +0100

    Added new file - decks.js

---
+++ js/decks.js
@@ -0,0 +1,546 @@
+const cpuDeck = [
+    {
+        title: "2-Club",
+        value: 2,
+        img: 'img/cards/cpuCards/cardClubs2.png'
+    },
+    {
+        title: "3-Club",
+        value: 3,
+        img: 'img/cards/cpuCards/cardClubs3.png'
+    },
+    {
+        title: "4-Club",
+        value: 4,
+        img: 'img/cards/cpuCards/cardClubs4.png'
+    },
+    {
+        title: "5-Club",
+        value: 5,
+        img: 'img/cards/cpuCards/cardClubs5.png'
+    },
+    {
+        title: "6-Club",
+        value: 6,
+        img: 'img/cards/cpuCards/cardClubs6.png'
+    },
+    {
+        title: "7-Club",
+        value: 7,
+        img: 'img/cards/cpuCards/cardClubs7.png'
+    },
+    {
+        title: "8-Club",
+        value: 8,
+        img: 'img/cards/cpuCards/cardClubs8.png'
+    },
+    {
+        title: "9-Club",
+        value: 9,
+        img: 'img/cards/cpuCards/cardClubs9.png'
+    },
+    {
+        title: "10-Club",
+        value: 10,
+        img: 'img/cards/cpuCards/cardClubs10.png'
+    },
+    {
+        title: "Jack-Club",
+        value: 11,
+        img: 'img/cards/cpuCards/cardClubsJ.png'
+    },
+    {
+        title: "Queen-Club",
+        value: 12,
+        img: 'img/cards/cpuCards/cardClubsQ.png'
+    },
+    {
+        title: "King-Club",
+        value: 13,
+        img: 'img/cards/cpuCards/cardClubsK.png'
+    },
+    {
+        title: "Ace-Club",
+        value: 14,
+        img: 'img/cards/cpuCards/cardClubsA.png'
+    },
+    {
+        title: "2-Diamond",
+        value: 2,
+        img: 'img/cards/cpuCards/cardDiamonds2.png'
+    },
+    {
+        title: "3-Diamond",
+        value: 3,
+        img: 'img/cards/cpuCards/cardDiamonds3.png'
+    },
+    {
+        title: "4-Diamond",
+        value: 4,
+        img: 'img/cards/cpuCards/cardDiamonds4.png'
+    },
+    {
+        title: "5-Diamond",
+        value: 5,
+        img: 'img/cards/cpuCards/cardDiamonds5.png'
+    },
+    {
+        title: "6-Diamond",
+        value: 6,
+        img: 'img/cards/cpuCards/cardDiamonds6.png'
+    },
+    {
+        title: "7-Diamond",
+        value: 7,
+        img: 'img/cards/cpuCards/cardDiamonds7.png'
+    },
+    {
+        title: "8-Diamond",
+        value: 8,
+        img: 'img/cards/cpuCards/cardDiamonds8.png'
+    },
+    {
+        title: "9-Diamond",
+        value: 9,
+        img: 'img/cards/cpuCards/cardDiamonds9.png'
+    },
+    {
+        title: "10-Diamond",
+        value: 10,
+        img: 'img/cards/cpuCards/cardDiamonds10.png'
+    },
+    {
+        title: "Jack-Diamond",
+        value: 11,
+        img: 'img/cards/cpuCards/cardDiamondsJ.png'
+    },
+    {
+        title: "Queen-Diamond",
+        value: 12,
+        img: 'img/cards/cpuCards/cardDiamondsQ.png'
+    },
+    {
+        title: "King-Diamond",
+        value: 13,
+        img: 'img/cards/cpuCards/cardDiamondsK.png'
+    },
+    {
+        title: "Ace-Diamond",
+        value: 14,
+        img: 'img/cards/cpuCards/cardDiamondsA.png'
+    },
+    {
+        title: "2-Heart",
+        value: 2,
+        img: 'img/cards/cpuCards/cardHearts2.png'
+    },
+    {
+        title: "3-Heart",
+        value: 3,
+        img: 'img/cards/cpuCards/cardHearts3.png'
+    },
+    {
+        title: "4-Heart",
+        value: 4,
+        img: 'img/cards/cpuCards/cardHearts4.png'
+    },
+    {
+        title: "5-Heart",
+        value: 5,
+        img: 'img/cards/cpuCards/cardHearts5.png'
+    },
+    {
+        title: "6-Heart",
+        value: 6,
+        img: 'img/cards/cpuCards/cardHearts6.png'
+    },
+    {
+        title: "7-Heart",
+        value: 7,
+        img: 'img/cards/cpuCards/cardHearts7.png'
+    },
+    {
+        title: "8-Heart",
+        value: 8,
+        img: 'img/cards/cpuCards/cardHearts8.png'
+    },
+    {
+        title: "9-Heart",
+        value: 9,
+        img: 'img/cards/cpuCards/cardHearts9.png'
+    },
+    {
+        title:"10-Heart",
+        value: 10,
+        img: 'img/cards/cpuCards/cardHearts10.png'
+    },
+    {
+        title:"Jack-Heart",
+        value: 11,
+        img: 'img/cards/cpuCards/cardHeartsJ.png'
+    },
+    {
+        title:"Queen-Heart",
+        value: 12,
+        img: 'img/cards/cpuCards/cardHeartsQ.png'
+    },
+    {
+        title:"King-Heart",
+        value: 13,
+        img: 'img/cards/cpuCards/cardHeartsK.png'
+    },
+    {
+        title:"Ace-Heart",
+        value: 14,
+        img: 'img/cards/cpuCards/cardHeartsA.png'
+    },
+    {
+        title:"2-Spade",
+        value: 2,
+        img: 'img/cards/cpuCards/cardSpades2.png'
+    },
+    {
+        title:"3-Spade",
+        value: 3,
+        img: 'img/cards/cpuCards/cardSpades3.png'
+    },
+    {
+        title:"4-Spade",
+        value: 4,
+        img: 'img/cards/cpuCards/cardSpades4.png'
+    },
+    {
+        title:"5-Spade",
+        value: 5,
+        img: 'img/cards/cpuCards/cardSpades5.png'
+    },
+    {
+        title:"6-Spade",
+        value: 6,
+        img: 'img/cards/cpuCards/cardSpades6.png'
+    },
+    {
+        title:"7-Spade",
+        value: 7,
+        img: 'img/cards/cpuCards/cardSpades7.png'
+    },
+    {
+        title:"8-Spade",
+        value: 8,
+        img: 'img/cards/cpuCards/cardSpades8.png'
+    },
+    {
+        title:"9-Spade",
+        value: 9,
+        img: 'img/cards/cpuCards/cardSpades9.png'
+    },
+    {
+        title:"10-Spade",
+        value: 10,
+        img: 'img/cards/cpuCards/cardSpades10.png'
+    },
+    {
+        title:"Jack-Spade",
+        value: 11,
+        img: 'img/cards/cpuCards/cardSpadesJ.png'
+    },
+    {
+        title:"Queen-Spade",
+        value: 12,
+        img: 'img/cards/cpuCards/cardSpadesQ.png'
+    },
+    {
+        title:"King-Spade",
+        value: 13,
+        img: 'img/cards/cpuCards/cardSpadesK.png'
+    },
+    {
+        title:"Ace-Spade",
+        value: 14,
+        img: 'img/cards/cpuCards/cardSpadesA.png'
+    },
+    {
+        title:"JokerA",
+        value: 15,
+        img: 'img/cards/cpuCards/cardJokerA.png'
+    },
+    {
+        title:"JokerB",
+        value: 15,
+        img: 'img/cards/cpuCards/cardJokerB.png'
+    },
+
+];
+const playerDeck = [
+    {
+        title: "2-Club",
+        value: 2,
+        img: 'img/cards/playerCards/cardClubs2.png'
+    },
+    {
+        title: "3-Club",
+        value: 3,
+        img: 'img/cards/playerCards/cardClubs3.png'
+    },
+    {
+        title: "4-Club",
+        value: 4,
+        img: 'img/cards/playerCards/cardClubs4.png'
+    },
+    {
+        title: "5-Club",
+        value: 5,
+        img: 'img/cards/playerCards/cardClubs5.png'
+    },
+    {
+        title: "6-Club",
+        value: 6,
+        img: 'img/cards/playerCards/cardClubs6.png'
+    },
+    {
+        title: "7-Club",
+        value: 7,
+        img: 'img/cards/playerCards/cardClubs7.png'
+    },
+    {
+        title: "8-Club",
+        value: 8,
+        img: 'img/cards/playerCards/cardClubs8.png'
+    },
+    {
+        title: "9-Club",
+        value: 9,
+        img: 'img/cards/playerCards/cardClubs9.png'
+    },
+    {
+        title: "10-Club",
+        value: 10,
+        img: 'img/cards/playerCards/cardClubs10.png'
+    },
+    {
+        title: "Jack-Club",
+        value: 11,
+        img: 'img/cards/playerCards/cardClubsJ.png'
+    },
+    {
+        title: "Queen-Club",
+        value: 12,
+        img: 'img/cards/playerCards/cardClubsQ.png'
+    },
+    {
+        title: "King-Club",
+        value: 13,
+        img: 'img/cards/playerCards/cardClubsK.png'
+    },
+    {
+        title: "Ace-Club",
+        value: 14,
+        img: 'img/cards/playerCards/cardClubsA.png'
+    },
+    {
+        title: "2-Diamond",
+        value: 2,
+        img: 'img/cards/playerCards/cardDiamonds2.png'
+    },
+    {
+        title: "3-Diamond",
+        value: 3,
+        img: 'img/cards/playerCards/cardDiamonds3.png'
+    },
+    {
+        title: "4-Diamond",
+        value: 4,
+        img: 'img/cards/playerCards/cardDiamonds4.png'
+    },
+    {
+        title: "5-Diamond",
+        value: 5,
+        img: 'img/cards/playerCards/cardDiamonds5.png'
+    },
+    {
+        title: "6-Diamond",
+        value: 6,
+        img: 'img/cards/playerCards/cardDiamonds6.png'
+    },
+    {
+        title: "7-Diamond",
+        value: 7,
+        img: 'img/cards/playerCards/cardDiamonds7.png'
+    },
+    {
+        title: "8-Diamond",
+        value: 8,
+        img: 'img/cards/playerCards/cardDiamonds8.png'
+    },
+    {
+        title: "9-Diamond",
+        value: 9,
+        img: 'img/cards/playerCards/cardDiamonds9.png'
+    },
+    {
+        title: "10-Diamond",
+        value: 10,
+        img: 'img/cards/playerCards/cardDiamonds10.png'
+    },
+    {
+        title: "Jack-Diamond",
+        value: 11,
+        img: 'img/cards/playerCards/cardDiamondsJ.png'
+    },
+    {
+        title: "Queen-Diamond",
+        value: 12,
+        img: 'img/cards/playerCards/cardDiamondsQ.png'
+    },
+    {
+        title: "King-Diamond",
+        value: 13,
+        img: 'img/cards/cpuCards/cardDiamondsK.png'
+    },
+    {
+        title: "Ace-Diamond",
+        value: 14,
+        img: 'img/cards/playerCards/cardDiamondsA.png'
+    },
+    {
+        title: "2-Heart",
+        value: 2,
+        img: 'img/cards/playerCards/cardHearts2.png'
+    },
+    {
+        title: "3-Heart",
+        value: 3,
+        img: 'img/cards/playerCards/cardHearts3.png'
+    },
+    {
+        title: "4-Heart",
+        value: 4,
+        img: 'img/cards/playerCards/cardHearts4.png'
+    },
+    {
+        title: "5-Heart",
+        value: 5,
+        img: 'img/cards/playerCards/cardHearts5.png'
+    },
+    {
+        title: "6-Heart",
+        value: 6,
+        img: 'img/cards/playerCards/cardHearts6.png'
+    },
+    {
+        title: "7-Heart",
+        value: 7,
+        img: 'img/cards/playerCards/cardHearts7.png'
+    },
+    {
+        title: "8-Heart",
+        value: 8,
+        img: 'img/cards/playerCards/cardHearts8.png'
+    },
+    {
+        title: "9-Heart",
+        value: 9,
+        img: 'img/cards/playerCards/cardHearts9.png'
+    },
+    {
+        title:"10-Heart",
+        value: 10,
+        img: 'img/cards/playerCards/cardHearts10.png'
+    },
+    {
+        title:"Jack-Heart",
+        value: 11,
+        img: 'img/cards/playerCards/cardHeartsJ.png'
+    },
+    {
+        title:"Queen-Heart",
+        value: 12,
+        img: 'img/cards/playerCards/cardHeartsQ.png'
+    },
+    {
+        title:"King-Heart",
+        value: 13,
+        img: 'img/cards/playerCards/cardHeartsK.png'
+    },
+    {
+        title:"Ace-Heart",
+        value: 14,
+        img: 'img/cards/playerCards/cardHeartsA.png'
+    },
+    {
+        title:"2-Spade",
+        value: 2,
+        img: 'img/cards/playerCards/cardSpades2.png'
+    },
+    {
+        title:"3-Spade",
+        value: 3,
+        img: 'img/cards/playerCards/cardSpades3.png'
+    },
+    {
+        title:"4-Spade",
+        value: 4,
+        img: 'img/cards/playerCards/cardSpades4.png'
+    },
+    {
+        title:"5-Spade",
+        value: 5,
+        img: 'img/cards/playerCards/cardSpades5.png'
+    },
+    {
+        title:"6-Spade",
+        value: 6,
+        img: 'img/cards/playerCards/cardSpades6.png'
+    },
+    {
+        title:"7-Spade",
+        value: 7,
+        img: 'img/cards/playerCards/cardSpades7.png'
+    },
+    {
+        title:"8-Spade",
+        value: 8,
+        img: 'img/cards/playerCards/cardSpades8.png'
+    },
+    {
+        title:"9-Spade",
+        value: 9,
+        img: 'img/cards/playerCards/cardSpades9.png'
+    },
+    {
+        title:"10-Spade",
+        value: 10,
+        img: 'img/cards/playerCards/cardSpades10.png'
+    },
+    {
+        title:"Jack-Spade",
+        value: 11,
+        img: 'img/cards/playerCards/cardSpadesJ.png'
+    },
+    {
+        title:"Queen-Spade",
+        value: 12,
+        img: 'img/cards/playerCards/cardSpadesQ.png'
+    },
+    {
+        title:"King-Spade",
+        value: 13,
+        img: 'img/cards/playerCards/cardSpadesK.png'
+    },
+    {
+        title:"Ace-Spade",
+        value: 14,
+        img: 'img/cards/playerCards/cardSpadesA.png'
+    },
+    {
+        title:"JokerA",
+        value: 15,
+        img: 'img/cards/playerCards/cardJokerA.png'
+    },
+    {
+        title:"JokerB",
+        value: 15,
+        img: 'img/cards/playerCards/cardJokerB.png'
+    },
+
+];
